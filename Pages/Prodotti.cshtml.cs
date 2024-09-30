14eb497f4cf875184c9c72dbef39204d924923fc
Pozzame <pozzame@gmail.com>
2024-09-30 16:01:54 +0200

    z

--- Pages/Prodotti.cshtml.cs
+++ Pages/Prodotti.cshtml.cs
@@ -16,14 +16,34 @@ namespace WebAppProdotti.Pages
 
         public IEnumerable<Prodotto> ElencoProdotti { get; set; }
 
-        public void OnGet()
+        public void OnGet(decimal? minPrezzo, decimal? maxPrezzo)
         {
-            ElencoProdotti = new List<Prodotto>
+            List<Prodotto> allProdotti = new List<Prodotto>
             {
                 new Prodotto { Id = 1, Nome = "Prod 1", Prezzo = 100, Dettaglio ="Dettaglio 1", Immagine = "./Images/1.jpg" },
                 new Prodotto { Id = 2, Nome = "Prod 2", Prezzo = 200, Dettaglio ="Dettaglio 2", Immagine = "./Images/2.jpg"},
                 new Prodotto { Id = 3, Nome = "Prod 3", Prezzo = 300, Dettaglio ="Dettaglio 3", Immagine = "./Images/3.jpg"}
             };
+
+            List<Prodotto> ElencoProdottiFiltrati = new List<Prodotto>();
+
+            foreach (var prodotto in allProdotti)
+            {
+                bool aggiungi = true;
+                if (minPrezzo.HasValue)
+                {
+                    if (prodotto.Prezzo < minPrezzo)
+                        aggiungi = false;
+                }
+                if (maxPrezzo.HasValue)
+                {
+                    if (prodotto.Prezzo > maxPrezzo)
+                        aggiungi = false;
+                }
+            if (aggiungi)
+                ElencoProdottiFiltrati.Add(prodotto);
+            }
+            ElencoProdotti = ElencoProdottiFiltrati;
         }
     }
 }
